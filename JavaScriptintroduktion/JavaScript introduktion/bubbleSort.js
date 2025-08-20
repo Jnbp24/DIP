13066c78c8a42d2136a172ba8269f2ffc686e8a8
Jonas-Petersen <jonasvm6@hotmail.com>
2025-08-20 09:56:53 +0200

    Fuck this node shit

---
+++ JavaScriptintroduktion/JavaScript introduktion/bubbleSort.js
@@ -0,0 +1,28 @@
+// bubbleSort.js
+let list = [7, 13, 9, 8, 4, 1, 2, 16, 0];
+
+for (let i = list.length - 1; i >= 0; i--) {
+    for (let j = 0; j <= i - 1; j++) {
+        if (list[j] > list[j + 1]) {
+            let temp = list[j];
+            list[j] = list[j+1];
+            list[j+1] = temp;
+        }
+    }
+}
+console.log(list.toString()); // => 0,1,2,4,7,8,9,13,16
+
+
+let listStrings = ["Dragonfruit", "Citrus", "Banana", "Apple", "Orange", "Kiwi"];
+
+for (let i = listStrings.length - 1; i >= 0; i--) {
+    for (let j = 0; j <= i - 1; j++) {
+        if (listStrings[j] > listStrings[j + 1]) {
+            let temp = listStrings[j];
+            listStrings[j] = listStrings[j+1];
+            listStrings[j+1] = temp;
+        }
+    }
+}
+
+console.log(listStrings);
